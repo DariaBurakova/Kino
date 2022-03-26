c8608632556569efb241b50e8a98d5abd521c04e
Dmitry <zik-element@mail.ru>
2022-03-26 19:37:06 +0300

    created registration

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,29 @@
 <template>
   <div id="app">
-    <button class="open" v-if="isVisbile" @click="isVisbile = !isVisbile">
-      Войти
-    </button>
-    <ListFilms v-else />
+    <Registration @VisbileCheng="VisbileCheng" :isVisbile="isVisbile" />
+    <ListFilms />
   </div>
 </template>
 
 <script>
 import ListFilms from "@/components/ListFilms";
+import Registration from "../components/Registrations.vue";
 
 export default {
   name: "HomeView",
 
   components: {
     ListFilms,
+    Registration,
   },
   data: () => ({
     isVisbile: true,
   }),
+  methods: {
+    VisbileCheng(check) {
+      this.isVisbile = check;
+    },
+  },
 };
 </script>
 
@@ -27,26 +32,5 @@ export default {
   //height: 60vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   background-color: black;
-}
-
-.open {
-  background-color: #d66c08;
-  border: none;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: #ffffff;
-  font-weight: 700;
-  font-size: 20px;
-  border-radius: 0 10px 0 10px;
-  width: 100px;
-  height: 50px;
-  margin: 45vh auto 0;
-}
-
-.open:hover {
-  transform: scale(1.02);
-  box-shadow: 0 14px 28px rgba(235, 225, 225, 0.25),
-    0 10px 10px rgba(248, 244, 244, 0.22);
 }
 </style>
