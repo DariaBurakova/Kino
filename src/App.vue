3ab38831d69b3407262a413971cdd05b7e27f0e7
Galand33rus <galand_@mail.ru>
2022-03-28 15:33:57 +0300

    add app-bar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,48 @@
 <template>
   <v-main>
-    <router-view />
+    <v-app-bar
+      color="deep-purple accent-4"
+      dense
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <img :src="logo" alt="logo">
+      <v-toolbar-title class="logo">VIDEOTEK</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <router-view/>
   </v-main>
 </template>
 
 <script>
+import logo from './assets/videotek.jpg'
 export default {
   name: 'App',
 
   data: () => ({
-    //
+    drawer: false,
+    group: null,
+    logo
   })
 }
 </script>
+
+<style scoped>
+img {
+  width: 50px;
+  height: 50px;
+}
+.logo {
+  font-family: 'Roboto', serif;
+  font-weight: 900;
+}
+</style>
