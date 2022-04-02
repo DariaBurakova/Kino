adbc74d85f1149cae7b7bb613930f6cdc96bf348
Dmitry <dp_140482@mail.ru>
2022-04-02 18:43:33 +0300

    Сборка и jest

--- tests/unit/Player.spec.js
+++ tests/unit/Player.spec.js
@@ -1,9 +1,27 @@
 import { mount } from '@vue/test-utils'
+import sinon from 'sinon'
 import Player from '../../src/components/Player.vue'
 
 describe('Player', () => {
+  const spy = sinon.stub()
   const wrapper = mount(Player)
   it('rendering', () => {
     expect(wrapper.html()).toContain('<div class="video">')
+  })
+  it('logging', () => {
+    const logSpy = jest.spyOn(console, 'log')
+    function A () {
+      console.log('')
+    }
+    A()
+    expect(logSpy).toHaveBeenCalled()
+  })
+  it('destroying', () => {
+    mount(Player, {
+      destroyed () {
+        spy()
+      }
+    }).destroy()
+    expect(spy.calledOnce).toBe(true)
   })
 })
